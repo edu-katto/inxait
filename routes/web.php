8ac8bd68258e7eaf472858eb2c16bcd8c966f4a8
Andres Sanchez <adsanchez@uniguajira.edu.co>
2022-08-16 09:42:26 -0500

    Se creo el controlador para procesar las peticiones de participantes y sus rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ParticipantesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('participantes', ParticipantesController::class)->except('show');
