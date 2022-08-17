39100f23d412bfc853a35a8350a8fddfec013d66
Andres Sanchez <adsanchez@uniguajira.edu.co>
2022-08-17 16:56:03 -0500

    Se creo la logica para capturar el token procesar la actualizacion y eliminar el token para que no se vuelva a validar

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -10,5 +10,20 @@ class EmailController extends Controller
 {
     public function emailConfirm($token){
 
+        $existe = Participantes::where('token_validacion' , $token)
+            ->count();
+
+        if ($existe >= 1){
+            Participantes::where('token_validacion' , $token)
+                ->update(['validacion' => '1', 'token_validacion' => '']);
+
+            return redirect()
+                ->route('inicio','#concurso')
+                ->with('success','Ya validamos tu correo, ya estas participando por un SUBARU XV!');
+        }
+
+        return redirect()
+            ->route('inicio','#concurso')
+            ->with('info','Ya este correo fue validado');
     }
 }
